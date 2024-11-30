96c681c12bab9687f13f6a3a0d2443bd2f9cd7da
CuongCoong <congngocuong1506@gmail.com>
2024-11-30 22:33:30 +0700

    RPS

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+let currentIndex = 0;
+const slider = document.getElementById('slider');
+const totalSlides = slider.children.length;
+
+function updateSlider() {
+	slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function nextSlide() {
+	currentIndex = (currentIndex + 1) % totalSlides;
+	updateSlider();
+}
+
+function prevSlide() {
+	currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+	updateSlider();
+}
